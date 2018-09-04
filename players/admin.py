53f9ef8b4e37794af038b77a9b936f830ef797bc
Matt Cappleman <matt.cappleman@gmail.com>
2018-09-04 17:43:08 +0000

    Ordering the players for player week admin

--- players/admin.py
+++ players/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player, PlayerWeek
+
+class PlayerWeekAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'player':
+            kwargs["queryset"] = Player.objects.order_by('name')
+        return super(PlayerWeekAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Player)
+admin.site.register(PlayerWeek, PlayerWeekAdmin)
--- players/models.py
+++ players/models.py
@@ -5,8 +5,10 @@ class Player(models.Model):
     name = models.CharField(max_length=200)
     position = models.CharField(max_length=4)
 
+
     def __str__(self):
         return self.name + ", " + self.position
+
 
     def valid_position(self):
         self.position = self.position.upper()
@@ -15,14 +17,23 @@ class Player(models.Model):
         return True
 
 
+    def save(self, *args, **kwargs):
+        if not self.valid_position():
+            raise ValueError('Not a valid position was given')
+
+        super().save(*args, **kwargs)
+
+
 class PlayerWeek(models.Model):
     player = models.ForeignKey(Player, on_delete=models.PROTECT)
     year = models.IntegerField(default=1999)
     week = models.IntegerField(default=0)
     points = models.FloatField(default=0)
 
+
     def __str__(self):
-        return self.player + ", " + self.year + ", " + self.week + ", " + self.points
+        return self.player.name + ", " + str(self.year) + ", " + str(self.week) + ", " + str(self.points)
+
 
     def valid_week(self):
         if self.week < 1 or self.week > 16:
