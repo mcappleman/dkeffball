f15b819f75a4c161ec7eb2d7ef6344eeeccb223e
Matt Cappleman <matt.cappleman@gmail.com>
2018-09-06 20:02:14 +0000

    Admin stuff and tests for matchups

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = 'DKE Fantasy Football Admin'
+admin.site.site_title = 'DKE Fantasy Football'
+
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
